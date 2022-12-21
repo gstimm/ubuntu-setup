a8f66ce5f0f7ba74617f5d03372d696cf6895998
Gabriel <contato.gabrieltimm@gmail.com>
2022-12-21 18:15:23 -0300

    chore: initial setup

---
+++ config_setup.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+cd ~
+sudo apt update
+sudo apt upgrade -y
+
+# Base
+sudo apt install gdebi-core wget curl apt-transport-https ca-certificates software-properties-common dconf-cli -y
+
+# Google Chrome Web Browser
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+rm -r ./google-chrome-stable_current_amd64.deb
+
+# Visual Studio Code
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+code --version
+
+# Git
+sudo apt install git
+git --version
+git config --global user.name "gstimm"
+git config --global user.email "contato.gabrieltimm@gmail.com"
+git config --global push.autoSetupRemote "true"
+git config --global pull.rebase "false"
+git config --global alias.s "git status -s"
+git config --global alias.c "git add . && git commit -m"
+
+# Zsh
+sudo apt install zsh -y
+zsh --version
+zsh
+chsh -s $(which zsh)
+
+# Oh My Zsh
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) -y"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+./install.sh && cd
+
+
+# NVM
+sudo apt -y install build-essential libssl-dev
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' | cat - .zshrc > temp && mv temp .zshrc
+nvm --version
+
+# Node LTS
+nvm install --lts
+node --version
+
+# Yarn
+npm install --global yarn
+yarn --version
+
+# Python
+sudo apt install python-is-python3
+sudo apt -y install python3-venv
+
+# Insomnia
+echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" \
+    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+sudo apt update
+sudo apt install insomnia
+
+# C Language
+sudo apt -y install gcc
+gcc --version
+
+# PostgreSQL
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install postgresql
+
+# PgAdmin4
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4
+
+# JDK
+sudo apt update
+sudo apt -y install default-jdk
+
+# DBeaver
+curl -fsSL https://dbeaver.io/debs/dbeaver.gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/dbeaver.gpg
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt update
+sudo apt install dbeaver-ce
+apt policy dbeaver-ce
+
+# Discord
+wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo gdebi ~/discord.deb
+
+# Docker
+sudo apt install gnome-terminal
+sudo apt remove docker-desktop
+rm -r $HOME/.docker/desktop
+sudo rm /usr/local/bin/com.docker.cli
+sudo apt purge docker-desktop
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+
+sudo usermod -aG docker ${USER}
+su - ${USER}
